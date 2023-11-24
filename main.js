75c5273ed86baf8cb8fb5dc9b8c8afca990c5825
mishthinagpal <105150035+mishthinagpal@users.noreply.github.com>
2023-11-24 17:35:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(600 , 600);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+
+   
+}
+function draw(){
+    image(video,50,50,500,500);
+   circle(60,60 ,100);
+   fill("red");
+   circle(60,550,100);
+   circle(550,60,100);
+   circle(550,550,100);
+   rectangle(60,60,60,10);
+}
+
+function take_snapshot(){
+    save('Mishthi.png')
+}
+
